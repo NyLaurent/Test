7b8ffc85b5f8b2e5548f6b8f0d35a573b56ba425
lauLanez <laulanyumbayire@gmail.com>
2024-06-03 21:37:00 -0500

    navbar component created

--- TEST/src/Components/Navbar.jsx
+++ TEST/src/Components/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
 
 const Navbar = () => {
-  return (
+    const [isMenuOPen, setIsMenuOpen]= useState(false);
+    const[isSticky,setIsSticky]= useState(false);
+
+
+    const toggleMenu =()=>{
+        setIsMenuOpen(!isMenuOPen);
+    }
+  
+    useEffect(()=>{
+        const handleScroll =()=>{
+            if(window.scrollY>100){
+                setIsSticky(true);
+            }
+            else{
+                setIsSticky(false);
+            }
+        }
+        window.addEventListener('scroll',handleScroll);
+        return ()=>{
+            window.addEventListener('scroll',handleScroll);
+        }
+        
+    })
+    return (
     <div>Navbar</div>
   )
 }
 
-export default Navbar
+export default Navbar  
