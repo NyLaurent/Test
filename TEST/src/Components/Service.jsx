edea150d6f6de1b22abd47d6478e4a1cb268c486
lauLanez <laulanyumbayire@gmail.com>
2024-06-05 00:03:41 -0500

    finished service component

--- TEST/src/Components/Service.jsx
+++ TEST/src/Components/Service.jsx
@@ -4,6 +4,19 @@ import logo2 from '../assets/logo2.png'
 import logo3 from '../assets/logo3.png'
 import logo4 from '../assets/logo4.png'
 const Service = () => {
+
+    const services =[
+        {id:1, title:"Membership Organisations", description:"Our membership management software provides full automation of membership renewals and payments",
+            image:'../assets/icon1.png'
+        },
+        {id:2, title:"National Associations", description:"Our membership management software provides full automation of membership renewals and payments",
+        image:'../assets/icon2.png'
+    },
+    {id:3, title:"Clubs And Groups", description:"Our membership management software provides full automation of membership renewals and payments",
+    image:'../assets/icon3.png'
+}
+
+    ]
   return (
     <div
     className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
@@ -22,6 +35,31 @@ const Service = () => {
 
             </div>
         </div>
+
+
+        <div className='mt-20 md:w-1/2 mx-auto text-center'>
+        <h2 className='text-4xl text-neutralDGrey font-semibold mb-3'>Manage your entire community in a 
+        single system</h2>
+            <p className='text-neutralDGrey '>Who is HINGA suitable for</p>
+
+        </div>
+        <div className='mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:w-11/12 mx-auto gap-12'>
+            {
+                services.map(service => <div key={services.id} className='px-4 py-8 text-center md:w-[300px] mx-auto md:h-80 rounded-md
+                shadow cursor-pointer hover:-translate-y-5 hover:border-b-4
+                hover:border-indigo-700 transition-all duration-300 flex items-center justify-center 
+                h-full'>
+
+                    <div>
+                        <div className='bg-[#E8F5E9] mb-4 h-14 w-14 mx-auto rounded-tl-3xl rounded-br-3xl'><img src={service.image} alt="" className='-ml-5'/></div>
+                        <h4 className='text-2xl font-bold text-neutralDGrey mb-2 px-2'>{service.title}</h4>
+                        <p className='text-sm text-neutralDGrey'>{service.description}</p>
+
+                    </div>
+                  </div>  )
+            }
+        </div>
+
     </div>
   )
 }
